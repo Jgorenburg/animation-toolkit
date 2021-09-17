80cd6d3e575345e038b8a2bb7de0843401911b6a
jacobgorenburg <jacobgorenburg@gmail.com>
2021-09-17 16:17:37 -0400

    fixed particles

--- assignments/a1-hello/particles.cpp
+++ assignments/a1-hello/particles.cpp
@@ -7,7 +7,7 @@ int numParticles = 100;
 struct particle {
   vec3 color;
   vec3 pos;
-  vec3 vel;
+  float vel;
 };
 
 class Particles : public atkui::Framework
@@ -17,17 +17,18 @@ class Particles : public atkui::Framework
   }
 
   virtual void setup() {
+    velocity = agl::randomUnitSphere();
     for(int i = 0; i < numParticles; i++){
         particles[i].color = agl::randomUnitCube() / 2.0f + vec3(0.5f);
         particles[i].pos = agl::randomUnitCube() * vec3(width(), height(), 0) + vec3(width() / 2, height() / 2, i);
-        particles[i].vel = agl::randomUnitCube() * vec3(width() / 300, height() / 300, 0);
+        particles[i].vel = agl::randomUnitCube().x / 2.0f + 0.5f;
     }
   }
 
   virtual void scene() {
     for(int i = 0; i < numParticles; i++){
       setColor(particles[i].color);
-      particles[i].pos += particles[i].vel;
+      particles[i].pos += velocity * particles[i].vel;
       if (particles[i].pos.x > width() || particles[i].pos.x < 0){
         particles[i].pos.x =  abs(abs(particles[i].pos.x) - width()); 
       }
@@ -41,6 +42,7 @@ class Particles : public atkui::Framework
  protected:
   particle particles [100];
   double lastTime = 0;
+  vec3 velocity;
 };
 
 int main(int argc, char** argv) {
